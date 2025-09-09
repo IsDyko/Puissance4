4d02690b60755be44a511bc62981a97ca2517c9b
Diogo Gonçalves Martins <pm71cef@eduvaud.ch>
2025-09-09 08:14:05 +0200

    Initialisation jeu avec création du menu

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace puissance4_Martins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuDemarrage();
+        }
+        
+        /// <summary>
+        /// Cette fonction sert à créer le menu principal du jeu
+        /// </summary>
+        static void MenuDemarrage()
+        {
+            int nombreLignes;
+            int nombreColonnes;
+            bool valeurOK;
+
+            Console.WriteLine("\t╔════════════════════════════════════════╗\n\t║   Bienvenue dans le jeu Puissance 4    ║\n\t║   Réalisé par Diogo Martins\t\t ║");
+            Console.WriteLine("\t╚════════════════════════════════════════╝");
+            Console.WriteLine("");
+            Console.Write("\tMerci d'entrer le nombre de lignes\n \tLa valeur doit être plus grande que ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(5);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" et plus petite que ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(13);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\n\tVotre valeur: ");
+
+            do
+            {
+                valeurOK = int.TryParse(Console.ReadLine(), out nombreLignes);
+
+                if (!valeurOK)
+                {
+                    Console.WriteLine("\tVotre valeur n'est pas un nombre ! Merci de réessayer !");
+                    Console.Write("\tVotre valeur: ");
+                }
+                else if (nombreLignes < 5 || nombreLignes > 13)
+                {
+                    Console.WriteLine("\tVotre valeur n'est pas dans les limites fixées > 5 et < 13. Merci de réessayer !");
+
+                    do
+                    {
+                        Console.Write("\tVotre valeur: ");
+                        valeurOK = int.TryParse(Console.ReadLine(), out nombreLignes);
+
+                        if (!valeurOK)
+                        {
+                            Console.WriteLine("\tVotre valeur n'es pas un chiffre. Réessayez");
+                            continue;
+                        }
+                        if (nombreLignes < 5 || nombreLignes > 13)
+                        {
+                            Console.WriteLine("\tVotre valeur n'est pas dans les limites fixées > 5 et < 13, Merci de réessayer !");
+                        }
+                    }
+                    while (nombreLignes < 5 || nombreLignes > 13 || !valeurOK);
+                }
+            }
+            while (!valeurOK);
+
+            Console.Write("\tMerci d'entrer le nombre de colonnes\n \tLa valeur doit être plus grande que ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(6);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" et plus petite que ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(16);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\n\tVotre valeur: ");
+
+            do
+            {
+                valeurOK = int.TryParse(Console.ReadLine(), out nombreColonnes);
+
+                if (!valeurOK)
+                {
+                    Console.WriteLine("\tVotre valeur n'est pas un nombre ! Merci de réessayer !");
+                    Console.Write("\tVotre valeur: ");
+                }
+                else if (nombreColonnes < 6 || nombreColonnes > 16)
+                {
+                    Console.WriteLine("\tVotre valeur n'est pas dans les limites fixées > 6 et < 16. Merci de réessayer !");
+
+                    do
+                    {
+                        Console.Write("\tVotre valeur: ");
+                        valeurOK = int.TryParse(Console.ReadLine(), out nombreColonnes);
+
+                        if (!valeurOK)
+                        {
+                            Console.WriteLine("\tVotre valeur n'es pas un chiffre. Réessayez");
+                            continue;
+                        }
+                        if (nombreColonnes < 6 || nombreColonnes > 16)
+                        {
+                            Console.WriteLine("\tVotre valeur n'est pas dans les limites fixées > 6 et < 16, Merci de réessayer !");
+                        }
+                    }
+                    while (nombreColonnes < 6 || nombreColonnes > 16 || !valeurOK);
+                }
+            }
+            while (!valeurOK);
+        }
+
+        static void genTableau()
+        {
+
+        }
+    }
+}
