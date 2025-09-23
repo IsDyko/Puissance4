e59ec988376477ac1687d8e53e9bf324b3db24a0
Diogo Gonçalves Martins <pm71cef@eduvaud.ch>
2025-09-23 10:43:17 +0200

    Fonction pour la génération du tableau

--- Program.cs
+++ Program.cs
@@ -12,23 +12,34 @@ namespace puissance4_Martins
         {
             int nombreColonnes;
             int nombreLignes;
+
+            CreationEnTete();
             MenuDemarrage(out nombreColonnes, out nombreLignes);
-            genTableau(nombreColonnes, nombreLignes);
+
+            Console.Clear();
+
+            CreationEnTete();
+            GenTableau(nombreColonnes, nombreLignes);
         }
 
         /// <summary>
-        /// Cette fonction sert à créer le menu principal du jeu
+        /// Fonction pour la création de l'entête
         /// </summary>
-        /// <param name="nombreColonnes"></param>
-        /// <param name="nombreLignes"></param>
-        static void MenuDemarrage(out int nombreColonnes, out int nombreLignes)
+        static void CreationEnTete()
         {
-            bool valeurOK;
-
             // Création de l'entête
             Console.WriteLine("\t╔════════════════════════════════════════╗\n\t║   Bienvenue dans le jeu Puissance 4    ║\n\t║   Réalisé par Diogo Martins\t\t ║");
             Console.WriteLine("\t╚════════════════════════════════════════╝");
             Console.WriteLine("");
+        }
+        /// <summary>
+        /// Cette fonction sert à créer le menu principal du jeu
+        /// </summary>
+        /// <param name="nombreColonnes">Ressort le nombre de colonnes choisi</param>
+        /// <param name="nombreLignes">Ressort le nombre de lignes choisi</param>
+        static void MenuDemarrage(out int nombreColonnes, out int nombreLignes)
+        {
+            bool valeurOK;
 
             //Nombre de lignes
             Console.Write("\tMerci d'entrer le nombre de lignes\n \tLa valeur doit être plus grande que ");
@@ -127,9 +138,9 @@ namespace puissance4_Martins
         /// <summary>
         /// Cette fonction permet la création et l'affichage du tableau
         /// </summary>
-        /// <param name="Colonnes"></param>
-        /// <param name="Lignes"></param>
-        static void genTableau(int Colonnes, int Lignes)
+        /// <param name="Colonnes">Prend le nombre de colonnes de l'utilisateur</param>
+        /// <param name="Lignes">Prend le nombre de lignes de l'utilisateur</param>
+        static void GenTableau(int Colonnes, int Lignes)
         {
             // Création d'un nouvel élement tableau
             char[,] tableau = new char[Lignes, Colonnes];
@@ -139,54 +150,157 @@ namespace puissance4_Martins
             {
                 for (int c = 0; c < Colonnes; c++)
                 {
-                    tableau[r, c] = '.';
+                    tableau[r, c] = ' ';
                 }
             }
 
+            // Boucles pour affichage de la barre de navigation
+            for (int r = 0; r < 1; r++)
+            {
+                //Ligne du haut
+                for (int c = 0; c < Colonnes; c++)
+                {
+                    if (c == 0)
+                    {
+                        Console.Write("\t╔═════╦");
+                    }
+                    else if (c < Colonnes - 1)
+                    {
+                        Console.Write("═════╦");
+                    }
+                    else if (c < Colonnes)
+                    {
+                        Console.Write("═════╗");
+                    }
+                }
+
+                Console.WriteLine();
+
+                //Ligne du milieu
+                for (int c = 0; c < Colonnes; c++)
+                {
+                    if (c == 0)
+                    {
+                        Console.Write("\t║  " + tableau[r, c] + "  ");
+                    }
+                    else if (c < Colonnes - 1)
+                    {
+                        Console.Write("║  " + tableau[r, c] + "  ");
+                    }
+                    else
+                    {
+                        Console.Write("║  " + tableau[r, c] + "  ║");
+                    }
+
+                }
+
+                Console.WriteLine();
+
+                //Ligne du bas
+                for (int c = 0; c < Colonnes; c++)
+                {
+                    {
+                        if (c == 0)
+                        {
+                            Console.Write("\t╚═════╩");
+                        }    
+                        else if (c < Colonnes - 1)
+                        {
+                            Console.Write("═════╩");
+                        }   
+                        else
+                        {
+                            Console.Write("═════╝");
+                        }   
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
             // Boucles pour affichage du tableau
+            // Boucle pour la première ligne
             for (int r = 0; r < Lignes; r++)
             {
                 for (int c = 0; c < Colonnes; c++)
                 {
                     if (c == 0 && r == 0)
                     {
-                        Console.Write("╔═════╦");
-                    }
+                        Console.Write("\t╔═════╦");    
+                    }    
                     else if (r == 0 && c < Colonnes - 1)
                     {
                         Console.Write("═════╦");
                     }
                     else if (r == 0 && c < Colonnes)
                     {
-                        Console.Write("═════╗");
-                    }
+                    Console.Write("═════╗");
+                    }    
                 }
+
                 Console.WriteLine();
-                for (int c = 0; c< Colonnes; c++)
+
+                // Boucle pour la ligne du milieu
+                for (int c = 0; c < Colonnes; c++)
                 {
-                    Console.Write("║  " + tableau[r, c] + "  ");
+                   if (c == 0)
+                   {
+                        Console.Write("\t║  " + tableau[r, c] + "  ");
+                   }
+                   else if (c < Colonnes - 1)
+                   {
+                        Console.Write("║  " + tableau[r, c] + "  ");
+                   }
+                   else
+                   {
+                        Console.Write("║  " + tableau[r, c] + "  ║");
+                   }
+
                 }
+
                 Console.WriteLine();
-                for(int c = 0; c < Colonnes; c++)
+
+                // Boucle pour la ligne du bas
+                for (int c = 0; c < Colonnes; c++)
                 {
-                    if (c == 0 && r < Lignes - 1)
-                    {
-                        Console.Write("╠═════╬");
-                    }
-                    else if (c > 0 && r < Lignes - 1)
-                    {
-                        Console.Write("═════╬");
-                    }
-                    else if (c == 0)
-                    {
-                        Console.Write("╚═════╩");
-                    }
-                    else
-                    {
-                        Console.Write("═════╩");
-                    }
+                   if (r < Lignes - 1)
+                   {
+                      if (c == 0)
+                      {
+                         Console.Write("\t╠═════╬"); // bord gauche
+                      }
+                      else if (c == Colonnes - 1)
+                      {
+                         Console.Write("═════╣"); // bord droit
+                      }
+                      else
+                      {
+                         Console.Write("═════╬"); // milieux
+                      }
+                   }
+                   else
+                   {
+                      if (c == 0)
+                      {
+                         Console.Write("\t╚═════╩"); // coin bas gauche
+                      }
+                      else if (c == Colonnes - 1)
+                      {
+                         Console.Write("═════╝"); // coin bas droit
+                      }
+                      else
+                      {
+                         Console.Write("═════╩"); // bas milieu
+                      }
+                   }
                 }
-            };
+            }
+        }
+
+        static void Gameplay()
+        {
+            
         }
     }
 }
